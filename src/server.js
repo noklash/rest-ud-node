14efe8b3bf9fa17e31ce52cdb6b0c74cfcaa0728
noklash <123834419+noklash@users.noreply.github.com>
2024-01-30 12:56:22 +0100

    added rate limiter

--- src/server.js
+++ src/server.js
@@ -6,6 +6,16 @@ import userRoutes from './user.routes';
 
 const app = express();
 const port = 3000;
+// API REATE LIMIT
+import { rateLimit } from 'express-rate-limit'
+
+const limiter = rateLimit({
+	windowMs:  60 * 1000, // 1 minute
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+})
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter)
 
 
 // this converts the response data  to a json object
